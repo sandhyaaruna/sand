41cac9a3a7b300b9703fb2f9039b2fadf348166f
sandhyaaruna <35653085+sandhyaaruna@users.noreply.github.com>
2018-01-21 16:58:08 +0530

    Update and rename svs.c to spk.c

---
+++ spk.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+  printf("enter the value of a");
+  scanf("%d",&a);
+  printf("enter the value of b");
+  scanf("%d",&b);
+  printf("enter the value of c");
+  scanf("%d",&c);
+if(a>b)
+{
+printf("%d",b);  
+}
+else
+{
+printf("%d",a);
+}
+if(c>b)
+{
+printf("%d",c);
+}
+  eise
+  {
+    printf("%d");
+  }
+  getch();
+}
