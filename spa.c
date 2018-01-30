4e8733463d63160ebe577fccfede7c20ff9161e7
sandhyaaruna <35653085+sandhyaaruna@users.noreply.github.com>
2018-01-30 16:16:58 +0530

    Create spa.c

---
+++ spa.c
@@ -0,0 +1,23 @@
+include<stdio.h>
+include<conio.h>
+void main()
+{
+int A[20];
+int i,n,k,sum=0;
+clrscr();
+printf("enter size of array");
+scanf("%d",&n);
+printf("enter k value to sum");
+scanf("%d",&k);
+printf("enter element of array ",n);
+for(i=o;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<k;i++)
+{
+sum=sum+A[i];
+}
+printf("sum of k values are %d",sum);
+getch();
+}
