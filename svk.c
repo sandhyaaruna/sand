efbc9273e8943428933aa3bd7b3e6cdf2b7044d4
sandhyaaruna <35653085+sandhyaaruna@users.noreply.github.com>
2018-01-30 16:50:15 +0530

    Rename san.c to svk.c

---
+++ svk.c
@@ -0,0 +1,24 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+int n,a=0,remainder,org int;
+printf("enter an int");
+scanf("%d",&n);
+original integer=n;
+while(n!=0)
+{
+rem=n%10
+rev int=rev in*10 +rem;
+n/=10;
+}
+if(org int==rev int)
+{
+printf("%d palindrome",org int);
+}
+else
+{
+printf("%d not palindrome", org int);
+}
+getch();
+}
