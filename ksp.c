28045305fdeae2aa8bf407b58175defaaa051869
sandhyaaruna <35653085+sandhyaaruna@users.noreply.github.com>
2018-01-30 16:37:27 +0530

    Create ksp.c

---
+++ ksp.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int b,e,i;
+long long pow=1;
+printf("enter base");
+scanf("%d",&b);
+printf("enter exponent");
+scanf("%d",&e);
+for(i=1;i<=exp;i++)
+{
+pow=pow*bare;
+getch();
+}
